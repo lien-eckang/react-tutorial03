c416e9debaa5464e03f63103a8fbb0d3c3017042
lien-eckang <euncho.kang@lien.kr>
2022-06-27 15:08:50 +0900

    Board 리팩토링
    - Board.jsx를 클래스스타일에서 함수스타일로 변환

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,35 +1,34 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Square from './Square';
 
-export default class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
+export default function Board(props) {
+  const { squares, onClick } = props;
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const renderSquare = (i) => (
+    <Square
+      value={squares[i]}
+      onClick={() => onClick(i)}
+    />
+  );
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
